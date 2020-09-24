931149ccbdfb5385b45ded90d4b19f9bc01e0964
zxhbloomer@gmail.com <Lyyzxh030324>
2020-09-24 09:37:01 +0800

    ..

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -7,7 +7,7 @@
         </el-menu-item>
       </app-link>
     </template>
-    <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
+    <el-submenu v-else ref="subMenu" :index="item.id + ''" popper-append-to-body>
       <template slot="title">
         <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title" />
       </template>
