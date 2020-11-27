9dc349e811c142c1cebb2c7ac0301116b0de737f
zxhbloomer@gmail.com <Lyyzxh030324>
2020-11-27 13:35:56 +0800

    ..

--- src/directive/permission/permission.js
+++ src/directive/permission/permission.js
@@ -26,22 +26,30 @@ export default {
    * @param {*} vnode
    */
   inserted(el, binding, vnode) {
-    debugger
     const { value } = binding
-    const roles = store.getters && store.getters.roles
 
-    if (value && value instanceof Array && value.length > 0) {
-      const permissionRoles = value
-
-      const hasPermission = roles.some(role => {
-        return permissionRoles.includes(role)
-      })
-
-      if (!hasPermission) {
-        el.parentNode && el.parentNode.removeChild(el)
-      }
-    } else {
-      throw new Error(`need roles! Like v-permission="['admin','editor']"`)
+    const userPermissionDataOperations = store.getters && store.getters.userPermissionData.permission_operation
+    const hasPermission = userPermissionDataOperations.find(item => item.operation_perms === value)
+    // 查找权限是否在操作数据中存在
+    debugger
+    if (!hasPermission) {
+      el.parentNode && el.parentNode.removeChild(el)
     }
+
+    // const roles = store.getters && store.getters.roles
+
+    // if (value && value instanceof Array && value.length > 0) {
+    //   const permissionRoles = value
+
+    //   const hasPermission = roles.some(role => {
+    //     return permissionRoles.includes(role)
+    //   })
+
+    //   if (!hasPermission) {
+    //     el.parentNode && el.parentNode.removeChild(el)
+    //   }
+    // } else {
+    //   throw new Error(`need roles! Like v-permission="['admin','editor']"`)
+    // }
   }
 }
--- src/views/20_master/group/group.vue
+++ src/views/20_master/group/group.vue
@@ -49,10 +49,10 @@
 
     <el-button-group>
       <el-button v-permission="'P_GROUP:ADD'" type="primary" icon="el-icon-circle-plus-outline" :loading="settings.loading" @click="handleInsert">新增</el-button>
-      <el-button :disabled="!settings.btnShowStatus.showUpdate" type="primary" icon="el-icon-edit-outline" :loading="settings.loading" @click="handleUpdate">修改</el-button>
-      <el-button :disabled="!settings.btnShowStatus.showCopyInsert" type="primary" icon="el-icon-camera-solid" :loading="settings.loading" @click="handleCopyInsert">复制新增</el-button>
-      <el-button :disabled="!settings.btnShowStatus.showExport" type="primary" icon="el-icon-s-management" :loading="settings.loading" @click="handleExport">导出</el-button>
-      <el-button :disabled="!settings.btnShowStatus.showUpdate" type="primary" icon="el-icon-info" :loading="settings.loading" @click="handleView">查看</el-button>
+      <el-button v-permission="'P_GROUP:UPDATE'" :disabled="!settings.btnShowStatus.showUpdate" type="primary" icon="el-icon-edit-outline" :loading="settings.loading" @click="handleUpdate">修改</el-button>
+      <el-button v-permission="'P_GROUP:COPY_INSERT'" :disabled="!settings.btnShowStatus.showCopyInsert" type="primary" icon="el-icon-camera-solid" :loading="settings.loading" @click="handleCopyInsert">复制新增</el-button>
+      <el-button v-permission="'P_GROUP:EXPORT'" :disabled="!settings.btnShowStatus.showExport" type="primary" icon="el-icon-s-management" :loading="settings.loading" @click="handleExport">导出</el-button>
+      <el-button v-permission="'P_GROUP:INFO'" :disabled="!settings.btnShowStatus.showUpdate" type="primary" icon="el-icon-info" :loading="settings.loading" @click="handleView">查看</el-button>
     </el-button-group>
     <el-table
       ref="multipleTable"
